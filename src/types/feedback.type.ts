0e6b814dd4da1d0db572b6c739808166b8ad82ea
Jayan Wadusinghearachchi <jayan.wadusinghearachchi@kognitiv.com>
2025-01-16 17:24:01 +0530

    message fix

--- src/types/feedback.type.ts
+++ src/types/feedback.type.ts
@@ -13,5 +13,11 @@ export const validationRules = [
     .withMessage("Name can contains alphabetical characters only")
     .trim(),
   body("customerEmail").isEmail().withMessage("Email format should be valid"),
-  body("comment").isString().withMessage("Comment should be a text").notEmpty().withMessage("comment is required"),
+  body("comment")
+    .isString()
+    .withMessage("Comment should be a text")
+    .notEmpty()
+    .withMessage("comment is required")
+    .isLength({ max: 1000, min: 20 })
+    .withMessage("Comment should be under min 20 and max 1000 characters"),
 ];
